cd94b6dbddf1b843f8f20ecd0cf49e6f77ac01fe
CeliaGallegoGomez <tiffanyona6@gmail.com>
2024-04-05 20:35:28 +0200

    actualizado

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 const buttonElement = document.getElementById("button-menu");
-const menuElement = document.getLemenById("menu");
+const menuElement = document.getElementById("menu");
 
 const handleMenu = () => {
   if (menuElement.classList.contains("show")) {
     menuElement.classList.remove("show");
   } else {
-    menuElement.classlist.add("show");
+    menuElement.classList.add("show");
   }
 };
 
